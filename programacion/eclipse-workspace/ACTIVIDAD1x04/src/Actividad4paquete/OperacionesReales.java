9d7a1998f645346223257822c82828e0a800b857
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-22 20:52:31 +0200

    unidad 1 actividad 4

---
+++ programacion/eclipse-workspace/ACTIVIDAD1x04/src/Actividad4paquete/OperacionesReales.java
@@ -0,0 +1,32 @@
+package Actividad4paquete;
+import java.util.Scanner;
+
+public class OperacionesReales {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			System.out.printf("¿Número real A? ");
+			final float NUMERO_A = input.nextFloat();
+			
+			System.out.printf("¿Número real B? ");
+			final float NUMERO_B = input.nextFloat();
+			
+			input.close();
+			
+			final float SUMA = NUMERO_A + NUMERO_B;
+			final float RESTA = NUMERO_A - NUMERO_B;
+			final float PRODUCTO = NUMERO_A * NUMERO_B;
+			final float DIVISION = NUMERO_A / NUMERO_B;
+			
+			System.out.printf(
+				NUMERO_A + " + " + NUMERO_B + " = " + SUMA + "\n" +
+				NUMERO_A + " - " + NUMERO_B + " = " + RESTA + "\n" +
+				NUMERO_A + " * " + NUMERO_B + " = " + PRODUCTO + "\n" +
+				NUMERO_A + " / " + NUMERO_B + " = " + DIVISION + "\n"
+			);
+		} catch (Exception e) {
+			System.out.printf("Ambos datos deben ser numeros reales.");
+		}
+	}
+}
