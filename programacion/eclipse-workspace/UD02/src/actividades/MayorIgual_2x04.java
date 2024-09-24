8163fe102fb437d513f38db848431530bcd13373
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-24 20:55:06 +0200

    actividad ud2 4

---
+++ programacion/eclipse-workspace/UD02/src/actividades/MayorIgual_2x04.java
@@ -0,0 +1,27 @@
+package actividades;
+import java.util.Scanner;
+
+/*
+ * este programa pide dos numeros por teclado
+ * e imprime por pantalla cuál es mayor, o si son iguales
+ */
+public class MayorIgual_2x04 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Primer número real: ");
+		double num1 = teclado.nextDouble();
+		
+		System.out.print("Segundo número real: ");
+		double num2 = teclado.nextDouble();
+		teclado.close();
+		
+		if (num1 > num2) {
+			System.out.printf("El número real %f es mayor", num1);
+		} else if (num2 > num1) {
+			System.out.printf("El número real %f es mayor", num2);
+		} else {
+			System.out.printf("Los números %f y %f son iguales", num1, num2);
+		}
+	}
+}
