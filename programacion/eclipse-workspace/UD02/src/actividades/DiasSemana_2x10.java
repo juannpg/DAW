d54d6eff893143745fa7eeff0f7049e4c6e3d9ca
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-25 11:51:02 +0200

    actividad ud2 10

--- programacion/eclipse-workspace/UD02/src/actividades/DiasSemana_2x10.java
+++ programacion/eclipse-workspace/UD02/src/actividades/DiasSemana_2x10.java
@@ -1,16 +1,22 @@
 package actividades;
 import java.util.Scanner;
 
-public class DiasSeamana {
+/*
+ * este programa pide un numero entero
+ * entre 1 y 7 e imprime por pantalla
+ * el dia de la seamana correspondiente
+ */
+public class DiasSemana_2x10 {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
 		String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
 		
+		System.out.print("Día de la semana: ");
 		int diaNum = teclado.nextInt();
 		teclado.close();
 		
-		if (diaNum <= 0 && diaNum > 7) {
+		if (diaNum <= 0 || diaNum > 7) {
 			System.out.print("El día de la semana debe estar entre 1 y 7");
 			return;
 		}
