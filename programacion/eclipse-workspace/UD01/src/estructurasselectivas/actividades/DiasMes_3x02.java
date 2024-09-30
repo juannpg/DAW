fbb2785e8732b5d2a0b6c98f05c530adf6e92fe6
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-30 16:20:11 +0200

    actividades 3x01 a 4

---
+++ programacion/eclipse-workspace/UD01/src/estructurasselectivas/actividades/DiasMes_3x02.java
@@ -0,0 +1,63 @@
+package estructurasselectivas.actividades;
+import java.util.Scanner;
+
+/*
+ * este mes pide por teclado un numero
+ * que representa un mes del año
+ * 
+ * si el numero no esta entre 1 y 12 imprime un error
+ * 
+ * imprime por pantalla el numero de dias de ese mes
+ */
+public class DiasMes_3x02 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("¿Mes? ");
+		int mes = teclado.nextInt();
+		teclado.close();
+		
+		if (mes < 1 || mes > 12) {
+			System.out.print("El mes debe estar comprendido entre 1 y 12.");
+		} else {
+			switch (mes) {
+				case 1:
+					System.out.print("31 días");
+					break;
+				case 2:
+					System.out.print("28 días");
+					break;
+				case 3:
+					System.out.print("31 días");
+					break;
+				case 4:
+					System.out.print("30 días");
+					break;
+				case 5:
+					System.out.print("31 días");
+					break;
+				case 6:
+					System.out.print("30 días");
+					break;
+				case 7:
+					System.out.print("31 días");
+					break;
+				case 8:
+					System.out.print("30 días");
+					break;
+				case 9:
+					System.out.print("31 días");
+					break;
+				case 10:
+					System.out.print("30 días");
+					break;
+				case 11:
+					System.out.print("31 días");
+					break;
+				case 12:
+					System.out.print("30 días");
+					break;
+			}
+		}
+	}
+}
---
+++ programacion/eclipse-workspace/UD01/src/estructurasselectivas/actividades/SumarSegundo_3x03.java
@@ -0,0 +1,63 @@
+package estructurasselectivas.actividades;
+import java.util.Scanner;
+
+/*
+ * este programa pide por teclado un numero de
+ * horas, minutos y segundos, y mientras esten
+ * entre 0 y 23, 0 y 60 y 0 y 60 respectivamente,
+ * les suma un segundo e imprime por consola
+ * el resultado
+ */
+public class SumarSegundo_3x03 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int tiempoTotal;
+		
+		System.out.print("¿Horas? ");
+		int horas = teclado.nextInt();
+		System.out.print("¿Minutos? ");
+		int mins = teclado.nextInt();
+		System.out.print("¿Segundos? ");
+		int segs = teclado.nextInt();
+		teclado.close();
+		
+		boolean error = false;
+		
+		if (horas < 0 || horas > 23) {
+			System.out.println("Las horas deben estar comprendidas entre 0 y 23.");
+			error = true;
+		}
+		
+		if (mins < 0 || mins > 59) {
+			System.out.println("Los minutos deben estar comprendidos entre 0 y 59.");
+			error = true;
+		}
+		
+		if (segs < 0 || segs > 59) {
+			System.out.println("Los segundos deben estar comprendidos entre 0 y 59.");
+			error = true;
+		}
+		
+		if (!error) {
+			int minsOut, horasOut, segsOut, dia;
+			
+			tiempoTotal = horas * 60 * 60 + mins * 60 + segs;
+			tiempoTotal += 1;
+			
+			minsOut = tiempoTotal / 60;
+			horasOut = minsOut / 60;
+		
+			minsOut = minsOut % 60;
+			segsOut = tiempoTotal % 60;
+			
+			if (horasOut >= 24) {
+				horas = 0;
+				System.out.printf("%dh %dm %ds + 1s = %d días %dh %dm %ds", horas, mins, segs, 1, horasOut, minsOut, segsOut);
+			} else {
+				System.out.printf("%dh %dm %ds + 1s = %dh %dm %ds", horas, mins, segs, horasOut, minsOut, segsOut);
+			}
+			
+		}
+	}
+}
