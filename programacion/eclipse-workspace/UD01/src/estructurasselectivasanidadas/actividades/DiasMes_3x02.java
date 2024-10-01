9ee30a47dfadb0b5f1dd3b7bfe64c12144284415
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-01 10:54:03 +0200

    correccion 3x02

--- programacion/eclipse-workspace/UD01/src/estructurasselectivasanidadas/actividades/DiasMes_3x02.java
+++ programacion/eclipse-workspace/UD01/src/estructurasselectivasanidadas/actividades/DiasMes_3x02.java
@@ -22,8 +22,9 @@ public class DiasMes_3x02 {
 			case 3:
 			case 5:
 			case 7:
-			case 9:
-			case 11:
+			case 8:
+			case 10:
+			case 12:
 				System.out.print("31 días");
 				break;
 			case 2:
@@ -31,9 +32,8 @@ public class DiasMes_3x02 {
 				break;
 			case 4:
 			case 6:
-			case 8:
-			case 10:
-			case 12:
+			case 9:
+			case 11:
 				System.out.print("30 días");
 				break;
 			default:
