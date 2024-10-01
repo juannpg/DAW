72075c19242f2555c89e5e819d2f3da0e3097561
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-01 12:24:08 +0200

    actividad 3x05

--- programacion/eclipse-workspace/UD01/src/estructurasselectivasanidadas/actividades/Cifras_3x05.java
+++ programacion/eclipse-workspace/UD01/src/estructurasselectivasanidadas/actividades/Cifras_3x05.java
@@ -5,8 +5,6 @@ public class Cifras_3x05 {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
-		int cifras;
-		
 		System.out.print("Número Entero (entre 0 y 99999)? ");
 		int num = teclado.nextInt();
 		teclado.close();
@@ -14,6 +12,7 @@ public class Cifras_3x05 {
 		if (num < 0 || num > 99999) {
 			System.out.print("El número entero debe estar comprendido entre 0 y 99999");
 		} else {
+			int cifras;
 			if (num < 10) {
 				cifras = 1;
 			} else if (num < 100) {
