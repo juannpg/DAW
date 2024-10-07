e76c52d72368bf4a5624b805776cd13ff4a7fb69
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-07 09:55:14 +0200

    mejora a4x07a

--- programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x07A_MaxMin.java
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x07A_MaxMin.java
@@ -5,7 +5,14 @@ public class A4x07A_MaxMin {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
-		int num, mayor = 0, menor = 0, contador = 0;
+		int num, mayor, menor, contador = 0;
+		
+		System.out.print("¿Número entero 0? ");
+		num = teclado.nextInt();
+		contador++;
+		
+		mayor = num;
+		menor = num;
 		
 		while (contador < 10) {
 			System.out.printf("¿Número entero %d? ", contador);
