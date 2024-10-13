8ff26290ff262eb6d9e22089a07d7092c393330a
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-13 17:16:32 +0200

    a4x14

---
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x14_NumeroSecreto.java
@@ -0,0 +1,38 @@
+package bucles.actividades;
+import java.util.Scanner;
+
+public class A4x14_NumeroSecreto {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		final int NUM_SECRETO = 19;
+		int numIntentos = 5;
+		int guess = 0;
+		boolean acierto = false;
+		
+		while (numIntentos > 0 && acierto == false) {
+			System.out.print("¿Número? ");
+			guess = teclado.nextInt();
+			numIntentos--;
+			
+			if (guess == NUM_SECRETO) {
+				acierto = true;
+			} else {
+				System.out.println("Has fallado el número secreto.");
+				
+				if (guess < NUM_SECRETO) {
+					System.out.printf("El número %d es menor que el número secreto.\n", guess);
+				} else {
+					System.out.printf("El número %d es mayor que el número secreto.\n", guess);
+				}
+			}
+		}
+		
+		teclado.close();
+		
+		if (acierto == true) {
+			System.out.print("Has acertado el número secreto.");
+		} else {
+			System.out.print("Has agotado los 5 intentos.");
+		}
+	}
+}
