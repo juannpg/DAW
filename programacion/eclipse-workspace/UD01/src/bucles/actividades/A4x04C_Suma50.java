6a858b499747a0383a01be06eb260986c7900a07
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-14 12:03:51 +0200

    auto push
    commit automatizado

---
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x04C_Suma50.java
@@ -0,0 +1,13 @@
+package bucles.actividades;
+
+public class A4x04C_Suma50 {
+	public static void main(String[] args) {
+		int suma = 0;
+		
+		for (int i = 1; i <= 50; i++) {
+			suma += i;
+		}
+		
+		System.out.printf("Suma: %d", suma);
+	}
+}
