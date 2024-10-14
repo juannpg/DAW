25db9d657cef64cfb70da1469a5c6e0f0c9e6ed1
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-14 10:31:16 +0200

    auto push
    commit automatizado

--- programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x14_NumeroSecreto.java
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x14_NumeroSecreto.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class A4x14_NumeroSecreto {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
+		
 		final int NUM_SECRETO = 19;
 		int numIntentos = 5;
 		int guess = 0;
 		boolean acierto = false;
 		
-		while (numIntentos > 0 && acierto == false) {
+		while (numIntentos > 0 && !acierto) {
 			System.out.print("¿Número? ");
 			guess = teclado.nextInt();
 			numIntentos--;
@@ -29,7 +30,7 @@ public class A4x14_NumeroSecreto {
 		
 		teclado.close();
 		
-		if (acierto == true) {
+		if (acierto) {
 			System.out.print("Has acertado el número secreto.");
 		} else {
 			System.out.print("Has agotado los 5 intentos.");
