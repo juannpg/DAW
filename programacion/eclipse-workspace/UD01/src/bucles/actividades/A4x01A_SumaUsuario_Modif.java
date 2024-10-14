79d4e189db3f468c619081390675a013a54d0b5e
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-14 11:09:04 +0200

    mejora nombres de ejercicios modif

--- programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x01A_SumaUsuario_Modif.java
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x01A_SumaUsuario_Modif.java
@@ -1,7 +1,7 @@
 package bucles.actividades;
 import java.util.Scanner;
 
-public class A4x01_SumaUsuario_Modif {
+public class A4x01A_SumaUsuario_Modif {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int num = 1, suma = 0;
