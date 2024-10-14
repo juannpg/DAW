37faf8e016baba5d999bf4178b10e710cb792e07
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-14 11:58:00 +0200

    auto push
    commit automatizado

--- programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x03B_CincuentaNaturales.java
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x03B_CincuentaNaturales.java
@@ -3,10 +3,11 @@ package bucles.actividades;
 public class A4x03B_CincuentaNaturales {
 	public static void main(String[] args) {
 		int num = 1;
+		final int LIMITE = 50;
 
 		do {
 			System.out.println("Numero natural: " + num);
 			num++;
-		} while (num <= 50);
+		} while (num <= LIMITE);
 	}
 }
