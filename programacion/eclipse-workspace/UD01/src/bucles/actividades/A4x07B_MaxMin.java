0981ff6af28cd4b17070e328224ffd06f4e1fa06
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-15 10:43:12 +0200

    correccion Ax04BC

--- programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x07B_MaxMin.java
+++ programacion/eclipse-workspace/UD01/src/bucles/actividades/A4x07B_MaxMin.java
@@ -5,25 +5,30 @@ public class A4x07B_MaxMin {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
-		int num, mayor = 0, menor = 0, contador = 0;
+		int num, mayor, menor, contador;
+		final int ITERACIONES = 10;
 		
+		System.out.print("¿Número entero 0? ");
+		num = teclado.nextInt();
+		
+		mayor = num;
+		menor = num;
+		
+		contador = 1;
 		do {
 			System.out.printf("¿Número entero %d? ", contador);
 			num = teclado.nextInt();
 			
 			if (num > mayor) {
 				mayor = num;
-			}
-			
-			if (num < menor) {
+			} else if (num < menor) {
 				menor = num;
 			}
 			
 			contador++;
-			
-		} while (contador < 10);
+		} while (contador < ITERACIONES);
 		
 		teclado.close();
-		System.out.printf("Mínimo %d\nMáximo %d", menor, mayor);
+		System.out.printf("Mínimo: %d\nMáximo: %d", menor, mayor);
 	}
 }
