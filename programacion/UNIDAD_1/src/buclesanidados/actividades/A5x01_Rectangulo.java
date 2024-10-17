2e39264f2d89a2da44d66275633b9493d6ca4dbb
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-17 09:53:21 +0200

    bucles anidados

---
+++ programacion/UNIDAD_1/src/buclesanidados/actividades/A5x01_Rectangulo.java
@@ -0,0 +1,34 @@
+package buclesanidados.actividades;
+import java.util.Scanner;
+
+public class A5x01_Rectangulo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("¿Altura del Rectángulo? ");
+        int altura = teclado.nextInt();
+        System.out.print("¿Anchura del Rectángulo? ");
+        int anchura = teclado.nextInt();
+        teclado.close();
+
+        boolean err = false;
+        if (altura <= 0) {
+            System.out.print("La altura del rectángulo debe ser positiva");
+            err = true;
+        }
+
+        if (anchura <= 0) {
+            System.out.print("La anchura del rectángulo debe ser positiva");
+            err = true;
+        }
+
+        if (!err) {
+            for (int i = 0; i < altura; i++) {
+                for (int j = 0; j < anchura; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
