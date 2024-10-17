4630bb6d0ca7a9a9b828b684b29bd78402afacd0
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-17 13:59:42 +0200

    mejora

--- programacion/UNIDAD_1/src/buclesanidados/actividades/A5x05_TrianguloNumeros.java
+++ programacion/UNIDAD_1/src/buclesanidados/actividades/A5x05_TrianguloNumeros.java
@@ -6,14 +6,14 @@ public class A5x05_TrianguloNumeros {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("¿Número límite del triángulo? ");
-        int limit = sc.nextInt();
+        final int LIMIT = sc.nextInt();
         sc.close();
 
-        if (limit <= 0) {
+        if (LIMIT <= 0) {
             System.out.println("El número límite debe ser positivo.");
         } else {
-            // este bucle crea las filas hasta el limite
-            for (int i = 1; i <= limit; i++) {
+            // este bucle crea las filas hasta el LIMITe
+            for (int i = 1; i <= LIMIT; i++) {
                 // este escribe los números de j a i, y escribe
                 // i al ser el numero de linea
                 for (int j = 1; j <= i; j++) {
