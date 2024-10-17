672d21505cd8ff650b9316e2283c30ad89169b1e
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-17 13:43:41 +0200

    a5x05

--- programacion/UNIDAD_1/src/buclesanidados/actividades/A5x05_TrianguloNumeros.java
+++ programacion/UNIDAD_1/src/buclesanidados/actividades/A5x05_TrianguloNumeros.java
@@ -9,14 +9,18 @@ public class A5x05_TrianguloNumeros {
         int limit = sc.nextInt();
         sc.close();
 
-        // este bucle crea las filas hasta el limite
-        for (int i = 1; i <= limit; i++) {
-            // este escribe los números de j a i, y escribe
-            // i al ser el numero de linea
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+        if (limit <= 0) {
+            System.out.println("El número límite debe ser positivo.");
+        } else {
+            // este bucle crea las filas hasta el limite
+            for (int i = 1; i <= limit; i++) {
+                // este escribe los números de j a i, y escribe
+                // i al ser el numero de linea
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
