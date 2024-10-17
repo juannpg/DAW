df431dc44202bbadfc5d6f17d94785d191b7964f
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-17 10:56:34 +0200

    a5x03

---
+++ programacion/UNIDAD_1/src/buclesanidados/actividades/A5x03_TrianguloReves.java
@@ -0,0 +1,23 @@
+package buclesanidados.actividades;
+import java.util.Scanner;
+
+public class A5x03_TrianguloReves {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("¿Altura del Triángulo? ");
+        int dimension = teclado.nextInt();
+        teclado.close();
+
+        int j;
+        for (int i = 0; i <= dimension; i++) {
+            for (j = dimension - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
