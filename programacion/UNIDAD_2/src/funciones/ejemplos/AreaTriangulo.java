170d0b07e1242b029847fadbd85dde4169dfb8d0
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-18 13:50:35 +0200

    ej

---
+++ programacion/UNIDAD_2/src/funciones/ejemplos/AreaTriangulo.java
@@ -0,0 +1,29 @@
+package funciones.ejemplos;
+import java.util.Scanner;
+
+public class AreaTriangulo {
+    /*
+     * calcula el area del triangulo en base a dos parametros
+     * base y altura
+     */
+    public static double areaTriangulo(double altura, double base) {
+        double area = (altura * base) / 2;
+        return area;
+    }
+
+    /*
+     * pide por teclado la base y altura de un triangulo
+     * para imprimir su area por consola
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿La altura del Triángulo? ");
+        int altura = sc.nextInt();
+        System.out.print("¿La base del Triángulo? ");
+        int base = sc.nextInt();
+        sc.close();
+
+        System.out.println("La área del Triángulo es: " + areaTriangulo(altura, base));
+    }
+}
