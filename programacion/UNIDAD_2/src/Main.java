28178a73a4bccb61abc113b94df6896c0a55f52f
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-18 13:43:59 +0200

    unidad 2

--- programacion/UNIDAD_2/src/Main.java
+++ programacion/UNIDAD_2/src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static double calcularSuma(int a, int b) {
+        double resultado = a + b;
+        return resultado;
+    }
 }
+
