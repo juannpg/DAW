71d9e7e280ce7522014b114952c16b17e3cb626d
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-21 09:28:17 +0200

    mejora

--- programacion/UNIDAD_2/src/funciones/actividades/A1x01_MinMax.java
+++ programacion/UNIDAD_2/src/funciones/actividades/A1x01_MinMax.java
@@ -2,21 +2,6 @@ package funciones.actividades;
 import java.util.Scanner;
 
 public class A1x01_MinMax {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("¿Número Entero A? ");
-        int a = sc.nextInt();
-        System.out.print("¿Número Entero B? ");
-        int b = sc.nextInt();
-        System.out.print("¿Número Entero C? ");
-        int c = sc.nextInt();
-        sc.close();
-
-        System.out.println("Mínimo: " + min(a, b, c));
-        System.out.println("Máximo: " + max(a, b, c));
-    }
-
     public static int min(int a, int b, int c) {
         int min = a;
 
@@ -43,5 +28,20 @@ public class A1x01_MinMax {
         }
 
         return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Número Entero A? ");
+        int a = sc.nextInt();
+        System.out.print("¿Número Entero B? ");
+        int b = sc.nextInt();
+        System.out.print("¿Número Entero C? ");
+        int c = sc.nextInt();
+        sc.close();
+
+        System.out.println("Mínimo: " + min(a, b, c));
+        System.out.println("Máximo: " + max(a, b, c));
     }
 }
