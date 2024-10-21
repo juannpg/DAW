0c667cf642a3b9db8dd9c8d885c80658d5d33c2c
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-21 09:48:23 +0200

    mejora

--- programacion/UNIDAD_2/src/funciones/actividades/A1x01_MinMax.java
+++ programacion/UNIDAD_2/src/funciones/actividades/A1x01_MinMax.java
@@ -8,9 +8,9 @@ public class A1x01_MinMax {
     public static int min(int a, int b, int c) {
         int min;
 
-        if (a < b && a < c) {
+        if (a <= b && a <= c) {
             min = a;
-        } else if (b < a && b < c) {
+        } else if (b <= a && b <= c) {
             min = b;
         } else {
             min = c;
@@ -25,9 +25,9 @@ public class A1x01_MinMax {
     public static int max(int a, int b, int c) {
         int max;
 
-        if (a > b && a > c) {
+        if (a >= b && a >= c) {
             max = a;
-        } else if (b > a && b > c) {
+        } else if (b >= a && b >= c) {
             max = b;
         } else {
             max = c;
