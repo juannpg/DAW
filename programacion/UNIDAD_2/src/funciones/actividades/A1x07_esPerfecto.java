fa3b97caf02c4cc02609fa2bff6bc5f9edf584d8
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-21 12:03:00 +0200

    a1x07

---
+++ programacion/UNIDAD_2/src/funciones/actividades/A1x07_esPerfecto.java
@@ -0,0 +1,34 @@
+package funciones.actividades;
+
+public class A1x07_esPerfecto {
+    /**
+     * acepta un parametro num entero, y devuelve
+     * si es un "numero perfecto"
+     * @param num
+     * @return
+     */
+    public static boolean esPerfecto(int num) {
+        int sumaDivisores = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sumaDivisores += i;
+            }
+        }
+
+        return num == sumaDivisores;
+    }
+
+    /**
+     * imprime por consola todos los numeros
+     * perfectos de 1 a un LIMITE
+     * @param args
+     */
+    public static void main(String[] args) {
+        final int LIMITE = 10000;
+        for (int i = 1; i <= LIMITE; i++) {
+            if (esPerfecto(i)) {
+                System.out.printf("El número %d es perfecto.\n", i);
+            }
+        }
+    }
+}
