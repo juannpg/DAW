7fc6b7d85e5cd1135c6cca7bb1988c28fa6b7fea
juannpg <juan.pasgut.workspace@gmail.com>
2024-10-22 13:06:38 +0200

    a2x01

---
+++ programacion/UNIDAD_2/src/procedimientos/actividades/A2x01_TablaMultiplicarNum.java
@@ -0,0 +1,33 @@
+package procedimientos.actividades;
+import java.util.Scanner;
+
+public class A2x01_TablaMultiplicarNum {
+    /**
+     * escribe la tabla de multiplicar de un entero
+     * @param num
+     */
+    public static void escribirTablaMultiplicar(int num) {
+        System.out.println("Tabla de multiplicar del número " + num);
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %2d = %2d\n", num, i, num * i);
+        }
+    }
+
+    /**
+     * pide un entero por teclado y llama al procedimiento
+     * de escribir la tabla de multiplicar
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Número? ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("El número debe ser positivo.");
+        } else {
+            escribirTablaMultiplicar(num);
+        }
+    }
+}
