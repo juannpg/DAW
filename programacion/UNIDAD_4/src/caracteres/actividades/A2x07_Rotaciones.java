8744ef6898200a7b7451813ad20c03e77390fd6e
juannpg <juan.pasgut.workspace@gmail.com>
2024-11-04 12:32:51 +0100

    a2x07

---
+++ programacion/UNIDAD_4/src/caracteres/actividades/A2x07_Rotaciones.java
@@ -0,0 +1,45 @@
+package caracteres.actividades;
+import entrada.Teclado;
+
+public class A2x07_Rotaciones {
+    public static String leerFrase() {
+        final int MAXIMO = 40;
+        final int MINIMO = 2;
+
+        String frase;
+        do {
+            frase = Teclado.leerCadena("¿Frase? ");
+            if (frase.length() < MINIMO || frase.length() > MAXIMO) {
+                System.out.println("La frase debe tener 5 caracteres como mínimo.\n" +
+                        "La frase debe tener 40 caracteres como máximo.");
+            }
+        } while (frase.length() <= MINIMO || frase.length() > MAXIMO);
+
+        return frase;
+    }
+
+    public static void rotarDerecha(String frase) {
+        System.out.println("rotaciones a la derecha");
+
+        for (int i = 0; i < frase.length(); i++) {
+            frase = frase.charAt(frase.length() - 1) + frase.substring(0, frase.length() - 1);
+            System.out.println(frase);
+        }
+    }
+
+    public static void rotarIzquierda(String frase) {
+        System.out.println("rotaciones a la izquierda");
+
+        for (int i = 0; i < frase.length(); i++) {
+            frase = frase.substring(1, frase.length() - 1) + frase.charAt(0);
+            System.out.println(frase);
+        }
+    }
+
+    public static void main(String[] args) {
+        String frase = leerFrase();
+
+        rotarDerecha(frase);
+        rotarIzquierda(frase);
+    }
+}
