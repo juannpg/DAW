1b5b144a2370c721a6749030efa514fd983ce00c
juannpg <juan.pasgut.workspace@gmail.com>
2024-11-04 11:54:36 +0100

    a2x05

--- programacion/UNIDAD_4/src/caracteres/actividades/A2x05_MenuFrases.java
+++ programacion/UNIDAD_4/src/caracteres/actividades/A2x05_MenuFrases.java
@@ -15,7 +15,37 @@ public class A2x05_MenuFrases {
         return opcion;
     }
 
+    public static String leerFrase() {
+        String frase = Teclado.leerCadena("Frase: ");
+        do {
+            if (frase.length() <= 0 || frase.length() > 60) {
+                System.out.println("La frase no puede ser vacía ni más de 60 caracteres");
+            }
+        } while (frase.length() <= 0 || frase.length() > 60);
+
+        return frase;
+    }
+
     public static void main(String[] args) {
-        int opcion = menu();
+        String frase;
+        int opcion;
+
+        do {
+            opcion = menu();
+            switch (opcion) {
+                case 1:
+                    frase = leerFrase();
+                    String mayusculas = frase.toUpperCase();
+                    System.out.println("Frase convertida a mayúsculas: " + mayusculas);
+                    break;
+                case 2:
+                    frase = leerFrase();
+                    String minusculas = frase.toLowerCase();
+                    System.out.println("Frase convertida a minúsculas: " + minusculas);
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        } while (opcion != 0);
     }
 }
