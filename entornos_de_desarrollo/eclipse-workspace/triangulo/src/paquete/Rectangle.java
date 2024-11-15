24de1b0a50928bedb4d9e44695ded15fbda01ffd
juannpg <juan.pasgut.workspace@gmail.com>
2024-11-15 12:26:16 +0100

    entornos polimorfismo & programacion a2x03

---
+++ entornos_de_desarrollo/eclipse-workspace/triangulo/src/paquete/Rectangle.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Rectangle extends Shape {
+	private int length;
+	private int width;
+	
+	public Rectangle(String color, int length, int width) {
+		this.color = color;
+		this.length = length;
+		this.width = width;
+	}
+	
+	
+}
