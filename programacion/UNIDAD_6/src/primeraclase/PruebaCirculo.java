338428f3e4d6fb4fd45c833b581e06b45217e98e
juannpg <juan.pasgut.workspace@gmail.com>
2024-11-18 09:42:53 +0100

    refactoring

--- programacion/UNIDAD_6/src/primeraclase/PruebaCirculo.java
+++ programacion/UNIDAD_6/src/primeraclase/PruebaCirculo.java
@@ -3,16 +3,13 @@ package primeraclase;
 public class PruebaCirculo {
     public static void main(String[] args) {
         Circulo c1 = new Circulo(3.4, "azul");
-
         System.out.println(c1.calcularArea());
 
         c1.setColor("rojo");
-
         System.out.println(c1.getColor());
         System.out.println(c1.calcularPerimetro());
 
         Circulo c2 = new Circulo(3);
-
         System.out.println(c2.getColor());
 
         Circulo c3 = new Circulo("verde");
