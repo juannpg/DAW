a4d4f26c963d2a6e7d19dbc5f3b5a3109a3a37a4
juannpg <juan.pasgut.workspace@gmail.com>
2024-12-26 18:06:51 +0100

    progress

--- programacion/UNIDAD_6/src/trabajonavidad/Video.java
+++ programacion/UNIDAD_6/src/trabajonavidad/Video.java
@@ -1,10 +1,9 @@
 package trabajonavidad;
 
+import java.util.GregorianCalendar;
 import java.util.Random;
 
 public abstract class Video {
-    private static int numElementos;
-
     private static final String[] TITULOS = {
             "El Mago de Oz",
             "Harry Potter",
@@ -24,9 +23,30 @@ public abstract class Video {
             "Documental"
     };
 
+    private static final String[] PAISES = {
+            "España",
+            "Francia",
+            "Italia",
+            "Alemania",
+            "Moldavia",
+            "Argentina",
+            "Reino Unido"
+    };
+
+    private static GregorianCalendar[] AÑOS = {
+            new GregorianCalendar(1990, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(1995, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(2000, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(2005, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(2010, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(2015, GregorianCalendar.JANUARY, 1),
+            new GregorianCalendar(2020, GregorianCalendar.JANUARY, 1)
+    };
+
     protected String codigo;
     protected String titulo;
     protected String genero;
+    protected String pais;
     protected int puntuacion;
 
     private static String generarTitulo() {
@@ -39,16 +59,24 @@ public abstract class Video {
         return Video.GENEROS[i];
     }
 
+    private static String generarPais() {
+        int i = new Random().nextInt(8);
+        return Video.PAISES[i];
+    }
 
     private static int generarPuntuacion() {
         return new Random().nextInt(10) + 1;
     }
 
-    public Video() {
-        this.codigo = "VID" + Video.numElementos;
+    public Video(int codigo) {
+        this.codigo = "VID" + codigo;
         this.titulo = generarTitulo();
         this.genero = generarGenero();
+        this.pais = generarPais();
         this.puntuacion = generarPuntuacion();
-        Video.numElementos++;
+    }
+
+    public static GregorianCalendar getAño(int i) {
+        return Video.AÑOS[i];
     }
 }
