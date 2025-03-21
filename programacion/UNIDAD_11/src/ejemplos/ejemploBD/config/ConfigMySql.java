8ace17d2ed37ef1ea6461aaa597d31b44fcf2aca
juannpg <juan.pasgut.workspace@gmail.com>
2025-03-21 16:47:16 +0100

    solicunoadasdin

--- programacion/UNIDAD_11/src/ejemplos/ejemploBD/config/ConfigMySql.java
+++ programacion/UNIDAD_11/src/ejemplos/ejemploBD/config/ConfigMySql.java
@@ -9,9 +9,16 @@ import java.sql.SQLException;
 public class ConfigMySql {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URLBD = "jdbc:mysql://localhost:3306/empresa";
+	public static final String NOMBRE_BD = "empresa";
 
-	private static final String usuario = "prompt";
-	private static final String contrasena = "Juanelvapers22";
+	private static final String usuario = "root";
+	private static String contrasena = "";
+
+	public static void setContrasena(String contrasena) throws BDException {
+		ConfigMySql.contrasena = contrasena;
+		Connection c = ConfigMySql.abrirConexion();
+		ConfigMySql.cerrarConexion(c);
+	}
 
 	public static Connection abrirConexion() throws BDException {
 		Connection conexion = null;
